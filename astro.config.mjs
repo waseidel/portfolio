c7d6287a2f95e3dc085386e761dff2e8bfcd7dfa
Walther Seidel <waseidev@gmail.com>
2024-07-12 17:07:13 +0200

    Starting all over again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,34 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://waseidel.github.io",
+	site: "https://waseidel.netlify.app",
+	integrations: [
+		preact(),
+		sitemap({
+			i18n: {
+				defaultLocale: "es",
+				locales: {
+					es: "es-ES",
+					en: "en-US",
+				},
+			},
+		}),
+	],
+	i18n: {
+		defaultLocale: "es",
+		locales: ["es", "en"],
+		fallback: {
+			en: "es",
+		},
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
+	output: "server",
+	adapter: netlify(),
 });
